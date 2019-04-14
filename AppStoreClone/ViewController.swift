4fd3f9675519e0b5ce84256df9c16b9d27a66c7c
dangar11 <tkach.igor.vladimirovich@gmail.com>
2019-04-14 14:00:28 +0300

    Start project

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Igor Tkach on 4/14/19.
+//  Copyright © 2019 Igor Tkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
