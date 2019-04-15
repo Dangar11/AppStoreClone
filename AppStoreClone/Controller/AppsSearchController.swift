eff81a4645defc4f081cccb4e4f36abd9caea172
dangar11 <tkach.igor.vladimirovich@gmail.com>
2019-04-15 14:52:57 +0300

    Implementing Search Bar Controller and Search Throttling

--- AppStoreClone/Controller/AppsSearchController.swift
+++ AppStoreClone/Controller/AppsSearchController.swift
@@ -15,7 +15,19 @@ class AppsSearchController: UICollectionViewController {
 
   
   fileprivate let cellId = "searchCell"
+  fileprivate let searchController = UISearchController(searchResultsController: nil)
+  
   fileprivate var appResults = [Result]()
+  
+  fileprivate let enterSearchTermLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Please enter search term above..."
+    label.textAlignment = .center
+    label.font = UIFont.boldSystemFont(ofSize: 20)
+    return label
+  }()
+  
+  var timer: Timer?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -23,16 +35,38 @@ class AppsSearchController: UICollectionViewController {
     collectionView.backgroundColor = .white
     collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
     
+    collectionView.addSubview(enterSearchTermLabel)
+    enterSearchTermLabel.fillSuperview(padding: .init(top: 100, left: 50, bottom: 0, right: 50))
+    enterSearchTermLabel.layer.opacity = 0
     
-    fetchItunesApps()
     
+    setupSearchBar()
+    
+    animateSearchLabel(opacity: 1)
+    
+  }
+  
+  
+  fileprivate func setupSearchBar() {
+    definesPresentationContext = true
+    navigationItem.searchController = self.searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+    searchController.dimsBackgroundDuringPresentation = false
+    searchController.searchBar.delegate = self
+  }
+  
+  
+  func animateSearchLabel(opacity: Float) {
+    UIView.animate(withDuration: 0.6) {
+      self.enterSearchTermLabel.layer.opacity = opacity
+    }
   }
   
   
   
   fileprivate func fetchItunesApps()  {
     
-    Service.shared.fetchApps { [unowned self] results, error  in
+    Service.shared.fetchApps(searchTerm: "") { [unowned self] results, error  in
       if let error = error {
         print("Failed to fetch apps:", error)
         return
@@ -82,6 +116,43 @@ extension AppsSearchController: UICollectionViewDelegateFlowLayout {
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return .init(width: view.frame.width, height: 350)
+  }
+  
+}
+
+
+extension AppsSearchController: UISearchBarDelegate {
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    
+    
+    //delay before performing the search
+    //throttling the search
+    
+    if searchText.isEmpty {
+      animateSearchLabel(opacity: 1)
+    } else {
+      animateSearchLabel(opacity: 0)
+    }
+    
+    
+    
+    timer?.invalidate()
+    
+    timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+      
+      //fire the search
+      Service.shared.fetchApps(searchTerm: searchText) { (result, error) in
+        self.appResults = result
+        DispatchQueue.main.async {
+          self.collectionView.reloadData()
+        }
+      }
+      
+    })
+    
+    
+    
   }
   
 }
--- AppStoreClone/Views/SearchResultCell.swift
+++ AppStoreClone/Views/SearchResultCell.swift
@@ -37,7 +37,6 @@ class SearchResultCell: UICollectionViewCell {
   
   let appIconImageView: UIImageView =  {
     let iv = UIImageView()
-    iv.backgroundColor = .red
     iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
     iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
     iv.layer.cornerRadius = 12
